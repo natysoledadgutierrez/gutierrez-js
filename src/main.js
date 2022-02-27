0be9ab9489af3485bf31c51f98012c8b5a6f01b8
Naty <natysoledadgutierrez@hotmail.com>
2022-02-27 19:08:41 -0300

    Se le agrega llaves a los condicionales

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 const proyectosFinales = [1, 3 ,5, 6, 8, 10];
 for (let i = 0; i <= 5; i++) {
     if (proyectosFinales[i] == 10) {
-        console.log('Sobresaliente ' + proyectosFinales[i])
+        console.log('Sobresaliente' + proyectosFinales [i]);
     }
-    else if(proyectosFinales[i] >= 6)
-        console.log('Aprobado con ' + proyectosFinales[i]);
-    else 
-        console.log('No Aprobado ')
+    else if(proyectosFinales[i] >= 6){
+        console.log('Aprobado' + proyectosFinales[i]); 
+    }
+    else {
+        console.log('No Aprobado'); 
+    }       
 }
