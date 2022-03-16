0c1adf8a3f26ec87e8a7fafe3ec859e7479cf920
Naty <natysoledadgutierrez@hotmail.com>
2022-03-16 00:46:59 -0300

    Se agregan variables necesarias, objetos de js, arrays y metodos de busqueda y filtrado

--- src/main.js
+++ src/main.js
@@ -1,50 +1,62 @@
-const producto1 = {
-    id: '1',
-    nombre: 'Monitor Samsung Led 22 Pulgadas Frecuencia Hz 60 Hdmi Full Hd Promo',
-    precio: 26599,
-    stock: 70,
-    cuotas: 12,
-    interes: 0,
-    envioGratis: false
-}
-
-const producto2 = {
-    id: '2',
-    nombre: 'Monitor Led 24 Pulgadas Iqual Iq24h 1080p Hdmi Full Hd',
-    precio: 20999,
-    stock: 40,
-    cuotas: 0,
-    interes: 0,
-    envioGratis: false
-}
-
-const producto3 = {
-    id: '3',
-    nombre: 'Auriculares gamer inalámbricos Astro A50 negro y gris',
-    precio: 30291,
-    stock: 5,
-    cuotas: 12,
-    interes: 25,
-    envioGratis: false
-}
-
-const producto4 = {
-    id: '4',
-    nombre: 'Gabinete Gamer Noga 8608 Coolers Panel Vidrio + Fuente',
-    precio: 8499,
-    stock: 2,
-    cuotas: 0,
-    interes: 0,
-    envioGratis: true
-}
-
 const agregarAlCarrito = (producto, cantidad) => {
     producto.cantidadQueLleva = cantidad;
     carrito.push(producto);
 }
 
+const borrarDelCarrito = (id) => {
+    let productoParaBorrar = carrito.filter(carrito => carrito.id === id);
+    carrito.shift(productoParaBorrar);
+}
+
+const tieneStock = (producto, cantidadQueLleva) => {
+    if (cantidadQueLleva < 1) {
+        return false;
+    }
+    else if (cantidadQueLleva > producto.stock) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+const buscarProducto = (valorDelBuscador) => {
+    let productosFiltrados = productos.filter(
+         (producto) => producto.nombre.toUpperCase().trim().includes(valorDelBuscador.toUpperCase().trim())
+    );
+    return productosFiltrados;
+}
+
+const obtenerOfertas = () => {
+    let productosFiltrados = productos.filter(
+         (producto) => producto.esOferta
+    );
+    return productosFiltrados;
+}
+
+const ordenarPorPrecioMenor = () => {
+    let productosOrdenados = productos.sort((a,b) => (a.precio > b.precio) ? 1 : ((b.precio > a.precio) ? -1 : 0));
+    return productosOrdenados;
+}
+
+const ordenarPorPrecioMayor = () => {
+    let productosOrdenados = productos.sort((a,b) => (a.precio < b.precio) ? 1 : ((b.precio < a.precio) ? -1 : 0));
+    return productosOrdenados;
+}
+
 const carrito = [];
-const productos = [producto1, producto2, producto3, producto4];
+const productos = [
+    { id: '1', nombre: 'Monitor Samsung Led 22 Pulgadas Frecuencia Hz 60 Hdmi Full Hd Promo', 
+    precio: 26599, stock: 70, cuotas: 12, interes: 0, envioGratis: false, esOferta: false },
+    {id: '2', nombre: 'Monitor Led 24 Pulgadas Iqual Iq24h 1080p Hdmi Full Hd', 
+    precio: 20999, stock: 40, cuotas: 0, interes: 0, envioGratis: false, esOferta: false },
+    {id: '3', nombre: 'Auriculares gamer inalámbricos Astro A50 negro y gris', 
+    precio: 30291, stock: 5, cuotas: 12, interes: 25, envioGratis: false, esOferta: true },
+    { id: '4', nombre: 'Gabinete Gamer Noga 8608 Coolers Panel Vidrio + Fuente', 
+    precio: 8499, stock: 2, cuotas: 0, interes: 0, envioGratis: true, esOferta: false },
+]
+
+//Iteracion Carrito Desafio Anterior (Incorporar Arrays)
 let seguir;
 
 while (seguir !== "n") {
@@ -82,14 +94,4 @@ for(let i=0; i < carrito.length; i++){
 
 alert(`La cantidad es de ${cantidadTotal} y el total es de $ ${total}`);
 
-function tieneStock(producto, cantidadQueLleva) {
-    if (cantidadQueLleva < 1) {
-        return false;
-    }
-    else if (cantidadQueLleva > producto.stock) {
-        return false;
-    }
-    else {
-        return true;
-    }
-}
+
